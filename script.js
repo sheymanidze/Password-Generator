399b1659e5ec92d3f29f16bef0a25b97e9e192e5
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-02 16:46:15 -0500

    function getRandom added

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var generateBtn = document.querySelector("#generate");
+
 var numbers = [48, 57];
 var upperCase = [65, 90];
 var lowerCase = [97, 122];
 var symbols = [33, 47];
-
-console.log(numbers, upperCase, lowerCase, symbols);
 
 // Write password to the #password input
 function writePassword() {
@@ -19,6 +18,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUpper());
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLower());
+
+function getRandomSymbol() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+console.log(getRandomSymbol());
+
 //connecting trigger and numbers in a box 
 var lengthRange = document.getElementById('lengthRange');
 console.log('range');
@@ -28,8 +47,8 @@ console.log('number');
 lengthRange.addEventListener('input', syncLength);
 lengthNumber.addEventListener('input', syncLength);
 
-function syncLength(event) {
-  var value = event.target.value;
+function syncLength(e) {
+  var value = e.target.value;
   lengthRange.value = value;
   lengthNumber.value = value;
 }
