d0e9c4fcfda8f9b59fccfb6f339ba56cb3b441fb
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-03-02 23:55:55 -0500

    prompted messages, button linked, alerts

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var upperCase = [65, 90];
 var lowerCase = [97, 122];
 var symbols = [33, 47];
 
+var functionsArray = [getRandomNumber(), getRandomUpper(), getRandomLower(), getRandomSymbol()]
+
+console.log(functionsArray)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,16 +16,50 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
+
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+var a = false;
+do {
+  var length = prompt("How many characters would you like to include?");
+  if (length < 8) {
+    alert("Minimum length is 8");
+  } else if (length > 128) {
+    alert("Maximum length is 128");
+  } else if (isNaN(length) === true) {
+    alert("Must be a number");
+  } else
+    a = true;
 }
-console.log(getRandomNumber());
+while (a === false);
+
+
+var chooseUpperCase = confirm("Would you like to include upper case letters?");
+
+var chooseLowerCase = confirm("Would you like to include lower case letters?");
+
+var numbersOnly = confirm("Would you like to include numbers?");
+
+var includeSymbols = confirm("Would you like to include symbols?");
+
+if (
+  chooseUpperCase === false &&
+  chooseLowerCase === false &&
+  includeSymbols === false &&
+  numbersOnly === false
+)
+
+  alert("Must select atleast one type of character")
+
+
+
+//generate random number, uppercase, lowercase, and symbol
+
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -33,22 +71,40 @@ function getRandomLower() {
 }
 console.log(getRandomLower());
 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumber());
+
 function getRandomSymbol() {
   return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 }
 console.log(getRandomSymbol());
 
-//connecting trigger and numbers in a box 
-var lengthRange = document.getElementById('lengthRange');
-console.log('range');
-var lengthNumber = document.getElementById('lengthNumber');
-console.log('number');
-
-lengthRange.addEventListener('input', syncLength);
-lengthNumber.addEventListener('input', syncLength);
-
-function syncLength(e) {
-  var value = e.target.value;
-  lengthRange.value = value;
-  lengthNumber.value = value;
+for (var i = 0; i <= length - 1; i++) {
+  console.log(i);
 }
+
+//message that tat least one type has to be selected
+
+//prompts
+
+
+
+//connecting trigger and numbers in a box 
+// var lengthRange = document.getElementById('lengthRange');
+
+// var lengthNumber = document.getElementById('lengthNumber');
+
+
+// lengthRange.addEventListener('input', syncLength);
+// lengthNumber.addEventListener('input', syncLength);
+
+// function syncLength(e) {
+//   var value = e.target.value;
+//   lengthRange.value = value;
+//   lengthNumber.value = value;
+// }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
